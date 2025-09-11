60cac39794b83025acc62540982138f4211544a3
Julien <pgyygp090@gmail.com>
2025-09-11 20:12:14 +0200

    updated 83

--- 99.hs
+++ 99.hs
@@ -1609,26 +1609,26 @@ k4 = Graph2 ['a', 'b', 'c', 'd'] [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')
 
 k4b = (['a', 'b', 'c', 'd'], [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a'), ('a', 'c'), ('b', 'd')])
 
-paths' :: (Eq a) => a -> a -> [(a, a)] -> [[a]]
-paths' a b xs | a == b = [[a]]
-              | otherwise = concat [map (a :) $ paths' d b $ [x | x <- xs, x /= (c, d)]
-                                   | (c, d) <- xs, c == a] ++ 
-                            concat [map (a :) $ paths' c b $ [x | x <- xs, x /= (c, d)]
-                                   | (c, d) <- xs, d == a]
-
-cycle' :: (Eq a) => a -> [(a, a)] -> [[a]]
-cycle' a xs = [a : path | e <- xs, fst e == a, path <- paths' (snd e) a [x | x <- xs, x /= e]] ++
-              [a : path | e <- xs, snd e == a, path <- paths' (fst e) a [x | x <- xs, x /= e]]
-
-spantree :: (Eq a) => Graph2 a -> [Graph2 a]
-spantree (Graph2 xs ys) = filter (not . cycles) $ filter (nodes) alltrees
-   where
-      alltrees = [Graph2 (ns edges) edges | edges <- foldr acc [[]] ys]
-      acc e es = es ++ (map (e:) es)
-      ns e = foldr (\x xs -> if x `elem` xs then xs else x:xs) 
-             [] $ concat $ map (\(a, b) -> [a, b]) e
-      nodes (Graph2 xs' ys') = length xs - 1 == length ys' && length xs' == length xs
-      cycles (Graph2 xs' ys') = any ((/=) 0 . length . flip cycle' ys') xs'
+--paths' :: (Eq a) => a -> a -> [(a, a)] -> [[a]]
+--paths' a b xs | a == b = [[a]]
+--              | otherwise = concat [map (a :) $ paths' d b $ [x | x <- xs, x /= (c, d)]
+--                                   | (c, d) <- xs, c == a] ++ 
+--                            concat [map (a :) $ paths' c b $ [x | x <- xs, x /= (c, d)]
+--                                   | (c, d) <- xs, d == a]
+--
+--cycle' :: (Eq a) => a -> [(a, a)] -> [[a]]
+--cycle' a xs = [a : path | e <- xs, fst e == a, path <- paths' (snd e) a [x | x <- xs, x /= e]] ++
+--              [a : path | e <- xs, snd e == a, path <- paths' (fst e) a [x | x <- xs, x /= e]]
+--
+--spantree :: (Eq a) => Graph2 a -> [Graph2 a]
+--spantree (Graph2 xs ys) = filter (not . cycles) $ filter (nodes) alltrees
+--   where
+--      alltrees = [Graph2 (ns edges) edges | edges <- foldr acc [[]] ys]
+--      acc e es = es ++ (map (e:) es)
+--      ns e = foldr (\x xs -> if x `elem` xs then xs else x:xs) 
+--             [] $ concat $ map (\(a, b) -> [a, b]) e
+--      nodes (Graph2 xs' ys') = length xs - 1 == length ys' && length xs' == length xs
+--      cycles (Graph2 xs' ys') = any ((/=) 0 . length . flip cycle' ys') xs'
 
 spantree2 :: ([Char], [(Char, Char)]) -> [([Char], [(Char, Char)])]
 spantree2 (xs, ys) = filter isConnected $ filter (noncycle) alltrees
@@ -1637,7 +1637,7 @@ spantree2 (xs, ys) = filter isConnected $ filter (noncycle) alltrees
       acc e es = es ++ (map (e:) es)
       uniqueval e = foldr (\x xs -> if x `elem` xs then xs else x:xs) 
              [] (concat $ map (\(a, b) -> [a, b]) e)
-      noncycle (xs', ys') = length xs - 1 == length ys' && (length $ unique (xs')) == length xs
+      noncycle (xs', ys') = length xs - 1 == length ys' -- && (length $ unique (xs')) == length xs
 
 --isCycle :: (Eq a) => ([a], [(a, a)]) -> Bool
 --isCycle ([], xs2) = False
